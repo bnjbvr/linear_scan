9e25f613ccbc5d7bc2d623d206a1524bacf1181f
Benjamin Bouvier <public@benj.me>
2020-01-09 17:17:30 +0100

    Update structure

--- src/linearscan.rs
+++ src/linearscan.rs
@@ -1,28 +1,11 @@
 pub use crate::api::*;
 
-#[path = "linearscan/compat.rs"]
 pub mod compat;
-
-#[path = "linearscan/allocator.rs"]
 mod allocator;
-
-#[path = "linearscan/api.rs"]
 mod api;
-
-#[path = "linearscan/flatten.rs"]
 mod flatten;
-
-#[path = "linearscan/gap.rs"]
 mod gap;
-
-#[path = "linearscan/generator.rs"]
 mod generator;
-
-#[path = "linearscan/graph.rs"]
 mod graph;
-
-#[path = "linearscan/json.rs"]
 mod json;
-
-#[path = "linearscan/liveness.rs"]
 mod liveness;
